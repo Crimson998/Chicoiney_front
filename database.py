1adc083ba2784285dbed50a7186ca8027041be8c
Crimson99898 <Crimson99898@gmail.com>
2025-07-22 00:54:51 -0400

    Use DATABASE_URL environment variable for database connection

--- database.py
+++ database.py
@@ -1,7 +1,8 @@
+import os
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "postgresql+asyncpg://postgres:qRWrnrkoNwCygDtzIZpLWXIAWOhxliqy@hopper.proxy.rlwy.net:41380/railway"
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_async_engine(
     DATABASE_URL, 
